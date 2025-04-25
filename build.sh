4651d63c2a22496570b7d49f68dd1884db7fbba9
DANG VIET DUNG <dungdv@sabitech.vn>
2025-04-25 21:51:43 +0700

    Add script clean and build

---
+++ build.sh
@@ -0,0 +1,11 @@
+composer install
+npm install
+npm run build
+php artisan clear-compiled
+php artisan route:clear
+php artisan event:clear
+php artisan config:clear
+php artisan view:clear
+php artisan cache:clear
+php artisan migrate
+php artisan storage:link
---
+++ clean.sh
@@ -0,0 +1,12 @@
+composer install
+npm install
+npm run build
+php artisan clear-compiled
+php artisan route:clear
+php artisan event:clear
+php artisan config:clear
+php artisan view:clear
+php artisan cache:clear
+php artisan migrate:fresh --seed
+php artisan storage:link
+php artisan orchid:admin
