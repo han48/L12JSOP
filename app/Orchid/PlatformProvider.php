8ded5779a3dce790bbfc2b7c2420ad7d5fee9459
dungdv <dungdv@sabitech.vn>
2025-04-25 13:43:22 +0700

    Update admin menu - Allow hide DEV menu with env DEV_MODE

--- app/Orchid/PlatformProvider.php
+++ app/Orchid/PlatformProvider.php
@@ -33,39 +33,56 @@ class PlatformProvider extends OrchidServiceProvider
      */
     public function menu(): array
     {
-        return [
-            Menu::make('Get Started')
-                ->icon('bs.book')
-                ->title('Navigation')
-                ->route(config('platform.index')),
+        $menu = [];
+        if (true === filter_var(env('DEV_MODE', false), FILTER_VALIDATE_BOOLEAN)) {
+            $menu = [
+                Menu::make('Get Started')
+                    ->icon('bs.book')
+                    ->title('Navigation')
+                    ->route(config('platform.index')),
 
-            Menu::make('Sample Screen')
-                ->icon('bs.collection')
-                ->route('platform.example')
-                ->badge(fn () => 6),
+                Menu::make('Sample Screen')
+                    ->icon('bs.collection')
+                    ->route('platform.example')
+                    ->badge(fn() => 6),
 
-            Menu::make('Form Elements')
-                ->icon('bs.card-list')
-                ->route('platform.example.fields')
-                ->active('*/examples/form/*'),
+                Menu::make('Form Elements')
+                    ->icon('bs.card-list')
+                    ->route('platform.example.fields')
+                    ->active('*/examples/form/*'),
 
-            Menu::make('Layouts Overview')
-                ->icon('bs.window-sidebar')
-                ->route('platform.example.layouts'),
+                Menu::make('Layouts Overview')
+                    ->icon('bs.window-sidebar')
+                    ->route('platform.example.layouts'),
 
-            Menu::make('Grid System')
-                ->icon('bs.columns-gap')
-                ->route('platform.example.grid'),
+                Menu::make('Grid System')
+                    ->icon('bs.columns-gap')
+                    ->route('platform.example.grid'),
 
-            Menu::make('Charts')
-                ->icon('bs.bar-chart')
-                ->route('platform.example.charts'),
+                Menu::make('Charts')
+                    ->icon('bs.bar-chart')
+                    ->route('platform.example.charts'),
 
-            Menu::make('Cards')
-                ->icon('bs.card-text')
-                ->route('platform.example.cards')
-                ->divider(),
+                Menu::make('Cards')
+                    ->icon('bs.card-text')
+                    ->route('platform.example.cards')
+                    ->divider(),
 
+                Menu::make('Documentation')
+                    ->title('Docs')
+                    ->icon('bs.box-arrow-up-right')
+                    ->url('https://orchid.software/en/docs')
+                    ->target('_blank'),
+
+                Menu::make('Changelog')
+                    ->icon('bs.box-arrow-up-right')
+                    ->url('https://github.com/orchidsoftware/platform/blob/master/CHANGELOG.md')
+                    ->target('_blank')
+                    ->badge(fn() => Dashboard::version(), Color::DARK),
+            ];
+        }
+
+        $adminMenu = [
             Menu::make(__('Users'))
                 ->icon('bs.people')
                 ->route('platform.systems.users')
@@ -77,19 +94,11 @@ class PlatformProvider extends OrchidServiceProvider
                 ->route('platform.systems.roles')
                 ->permission('platform.systems.roles')
                 ->divider(),
-
-            Menu::make('Documentation')
-                ->title('Docs')
-                ->icon('bs.box-arrow-up-right')
-                ->url('https://orchid.software/en/docs')
-                ->target('_blank'),
-
-            Menu::make('Changelog')
-                ->icon('bs.box-arrow-up-right')
-                ->url('https://github.com/orchidsoftware/platform/blob/master/CHANGELOG.md')
-                ->target('_blank')
-                ->badge(fn () => Dashboard::version(), Color::DARK),
         ];
+
+        $menu = array_merge($menu, $adminMenu);
+
+        return $menu;
     }
 
     /**
