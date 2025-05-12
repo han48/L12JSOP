3129a3ab07e9330542a04e2011dd6c2f09dcd3f9
DANG VIET DUNG <dungdv@sabitech.vn>
2025-05-12 21:32:46 +0700

    Fix bug register check term and policy

--- resources/views/vendor/orchid-fortify/auth/register.blade.php
+++ resources/views/vendor/orchid-fortify/auth/register.blade.php
@@ -4,6 +4,12 @@
 @section('content')
 
     <h1 class="h4 text-black mb-4">{{ __('Register') }}</h1>
+    <script>
+        function showButtonRegister(ctrl) {
+            console.log(ctrl.checked);
+            document.getElementById('btn-register-container').classList.toggle('d-none');
+        }
+    </script>
 
     <form role="form" method="POST" data-controller="form"
         data-turbo="{{ var_export(Str::startsWith(request()->path(), config('platform.prefix'))) }}" data-action="form#submit"
@@ -28,7 +34,14 @@
             {!! \Orchid\Screen\Fields\Password::make('password_confirmation')->title('Confirm Password')->autocomplete('new-password')->required()->placeholder(__('Enter password')) !!}
         </div>
 
-        <div class="row align-items-center">
+        <div class="form-check form-switch py-2">
+            <input class="form-check-input" type="checkbox" role="switch" id="terms" name="terms" onchange="showButtonRegister(this)" required />
+            <label class="form-check-label" for="terms">
+                {!! __('user.i_agree_to_the_terms_of_service_and_privacy_policy', ['url_terms' => route('terms.show'), 'url_policy' => route('policy.show')]) !!}
+            </label>
+        </div>
+
+        <div class="row align-items-center d-none" id="btn-register-container">
             <div class="ml-auto col-md-6 col-xs-12">
                 <button id="button-login" type="submit" class="btn btn-default">
                     {{ __('Register') }}
