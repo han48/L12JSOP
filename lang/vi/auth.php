c0bcb535d4d8fbc3bc08dbd3c4bbbeed2eda18ef
MR4-SFB3\Mr4 <zzz4everzzz@live.co.uk>
2026-01-05 00:13:09 +0700

    Add message for Vietnamese

---
+++ lang/vi/auth.php
@@ -0,0 +1,22 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Authentication Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used during authentication for various
+    | messages that we need to display to the user. You are free to modify
+    | these language lines according to your application's requirements.
+    |
+    */
+
+    'failed' => 'Thông tin đăng nhập không khớp với hồ sơ của chúng tôi.',
+    'password' => 'Mật khẩu được cung cấp không chính xác.',
+    'throttle' => 'Quá nhiều lần thử đăng nhập. Vui lòng thử lại sau :seconds giây.',
+    'two_factor_required' => 'Vui lòng xác nhận quyền truy cập tài khoản của bạn bằng cách nhập mã xác thực được cung cấp bởi ứng dụng xác thực của bạn.',
+    'two_factor_invalid' => 'Mã xác thực hai yếu tố được cung cấp không hợp lệ.',
+
+];
